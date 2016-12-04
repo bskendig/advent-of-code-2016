6163adecab951358fde582ab6a33bd52ab19e7bb
Brian Kendig <brian@mac.com>
2016-12-04 17:50:44 -0500

    4 done

--- 4/4/main.swift
+++ 4/4/main.swift
@@ -14,38 +14,50 @@ func getInput() -> String {
 }
 
 func checksum(_ name: String) -> String {
-    let characters = name.characters.filter({$0 != "-"})
-    var freq: [Character : Int] = [:]
-    for character in characters {
-        if freq[character] == nil { freq[character] = 1 }
-        else { freq[character]! += 1 }
+    var frequencies: [Character : Int] = [:]
+    for character in name.characters.filter({$0 != "-"}) {
+        if frequencies[character] == nil { frequencies[character] = 1 }
+        else { frequencies[character]! += 1 }
     }
-    var s: [Character] = Array(freq.keys).sorted(by: { k1, k2 in
-        if freq[k1] != freq[k2] { return freq[k1]! > freq[k2]! }
-        else { return k1 < k2 }
+    var sortedCharacters: [Character] = Array(frequencies.keys).sorted(by: { c1, c2 in
+        if frequencies[c1] != frequencies[c2] { return frequencies[c1]! > frequencies[c2]! }
+        else { return c1 < c2 }
     })
-    return String(s[0...4])
+    return String(sortedCharacters[0...4])  // assuming there are always at least five different characters
 }
 
-func sectorId(_ room: String) -> Int {
+func decrypt(_ s: String, key: Int) -> String {
+    let start = UnicodeScalar("a").value
+    let s2: [UnicodeScalar] = s.unicodeScalars.map { c -> UnicodeScalar in
+        if c == "-" { return " " }
+        let newOffset = (c.value - start + UInt32(key)) % 26
+        return UnicodeScalar(start + newOffset)!
+    }
+    return s2.map({ $0.description }).joined()
+}
+
+func room(_ room: String) -> (name: String, id: Int)? {
     let regex = try! NSRegularExpression(pattern: "^(.+)-(\\d+)\\[(.+)\\]$", options: [])
     let matches = regex.matches(in: room, options: [], range: NSMakeRange(0, room.characters.count))
-    if matches.isEmpty { return 0 }
+    if matches.isEmpty { return nil }
     let name = (room as NSString).substring(with: matches[0].rangeAt(1))
     let id = Int((room as NSString).substring(with: matches[0].rangeAt(2)))!
     let cksum = (room as NSString).substring(with: matches[0].rangeAt(3))
-    print("\(name), \(id), \(cksum)")
-    if cksum == checksum(name) { return id }
-    else { return 0 }
+    if cksum == checksum(name) { return (decrypt(name, key: id), id) }
+    else { return nil }
 }
 
 func main() {
-    let rooms = getInput().components(separatedBy: "\n")
+    let entries = getInput().components(separatedBy: "\n")
     var sum = 0
-    for room in rooms {
-        sum += sectorId(room)
+    for entry in entries {
+        guard let r = room(entry) else {
+            continue
+        }
+        sum += r.id
+        print("\(r.id): \(r.name)")
     }
-    print(sum)
+    print("Sum of real room IDs is \(sum).")
 }
 
 main()
