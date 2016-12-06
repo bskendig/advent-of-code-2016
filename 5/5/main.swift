c36d8052ebc0530493d7b94f1eb23a29f643a7b0
Brian Kendig <brian@mac.com>
2016-12-05 23:48:06 -0500

    5 with autorelease and timing

--- 5/5/main.swift
+++ 5/5/main.swift
@@ -11,11 +11,9 @@ import Foundation
 func md5(_ s: String) -> Data? {
     guard let message = s.data(using: String.Encoding.utf8) else { return nil }
     var digest = Data(count: Int(CC_MD5_DIGEST_LENGTH))
-    autoreleasepool {
-        _ = digest.withUnsafeMutableBytes { digestBytes in
-            message.withUnsafeBytes { messageBytes in
-                CC_MD5(messageBytes, CC_LONG(message.count), digestBytes)
-            }
+    _ = digest.withUnsafeMutableBytes { digestBytes in
+        message.withUnsafeBytes { messageBytes in
+            CC_MD5(messageBytes, CC_LONG(message.count), digestBytes)
         }
     }
     return digest
@@ -27,9 +25,15 @@ func main() {
     var password1 = [String](repeating: " ", count: passwordLength), offset1 = 0
     var password2 = [String](repeating: " ", count: passwordLength), donePassword2 = false
     var index = 0
+    let start = NSDate().timeIntervalSince1970
+    print("Starting...")
     while offset1 < passwordLength || !donePassword2 {
         let key = doorID + String(index)
-        guard let data = md5(key) else { break }
+        var d: Data?
+        autoreleasepool {
+            d = md5(key)
+        }
+        guard let data = d else { break }
         if data[0] == 0x00 && data[1] == 0x00 && data[2] < 0x10 {
             // part 1
             if offset1 < passwordLength {
@@ -45,8 +49,9 @@ func main() {
         }
         index += 1
     }
-    print(password1.joined())
-    print(password2.joined())
+    print("First password: \(password1.joined())")
+    print("Second password: \(password2.joined())")
+    print("Finished in \(Int(Date().timeIntervalSince1970 - start)) seconds.")
 }
 
 main()
